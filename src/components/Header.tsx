f51e452301e5d855951759613fbd5852e7d46922
Beby Kurniawan <bebykurniawan2006@gmail.com>
2025-02-02 20:40:39 +0700

    feat(header): build noto's header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import Search from "./Search"
+
+const Header = () => {
+  return (
+    <div className="p-10 flex justify-between">
+      <h1 className="text-3xl font-medium text-white">📋Noto</h1>
+      <Search/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Search.tsx
@@ -0,0 +1,10 @@
+
+const Search = () => {
+  return (
+    <>
+     <input type="text"placeholder="Search" className="text-white border p-2"/>
+    </>
+  )
+}
+
+export default Search
